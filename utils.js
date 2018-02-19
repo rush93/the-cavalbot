bbdf7a1ddb83b664959596d6a451404f13d0ef9b
yann dardot <yann@donkeycode.com>
2018-02-19 14:54:45 +0100

    generate clan images

--- utils.js
+++ utils.js
@@ -91,5 +91,26 @@ module.exports = {
             return isPS4 ? playerName + ' ' +  ps4text : playerName;
         }
         return input;
+    },
+    getScoreOfClan(Players, clanId) {
+
+        var scores = {};
+        var players = Players.getPlayers();
+        var PlayerKeys = Object.keys(players);
+        for (var i = 0; i < PlayerKeys.length; i++) {
+            var objectKeys = Object.keys(players[PlayerKeys[i]].clans);
+            for (var j = 0; j < objectKeys.length; j++) {
+                if (typeof(players[PlayerKeys[i]].clans[objectKeys[j]]) === "object") {
+                    if (!scores[objectKeys[j]]) {
+                        scores[objectKeys[j]] = 0;
+                    }
+                    if (players[PlayerKeys[i]].clans[objectKeys[j]]) {
+                        scores[objectKeys[j]]+= players[PlayerKeys[i]].clans[objectKeys[j]].points;
+                    }
+                }
+            }
+        }
+
+        return scores[clanId];
     }
 }
