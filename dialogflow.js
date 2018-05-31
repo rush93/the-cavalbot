12c3866a4aa3fdb42de615398cd7ed1fc846dd0e
yann dardot <yann@donkeycode.com>
2018-05-31 13:45:14 +0200

    add dialogflow

---
+++ dialogflow.js
@@ -0,0 +1,38 @@
+const projectId = 'small-talk-9b89f';
+const sessionId = 'quickstart-session-id';
+const languageCode = 'fr-FR';
+
+// Instantiate a DialogFlow client.
+const dialogflow = require('dialogflow');
+const sessionClient = new dialogflow.SessionsClient({
+    keyFilename: 'dialogflow-credentials.json'
+  });
+
+const sessions = {}
+
+module.exports =  (userId, message) => {
+    if (!sessions[userId]) {
+        sessions[userId] = sessionClient.sessionPath(projectId, userId);
+    }
+    const request = {
+        session: sessions[userId],
+        queryInput: {
+          text: {
+            text: message,
+            languageCode: languageCode,
+          },
+        },
+      };
+    return new Promise((resolve, reject) => {
+        sessionClient
+        .detectIntent(request)
+        .then(responses => {
+            const result = responses[0].queryResult;
+            console.log(result);
+            resolve(result.fulfillmentText);
+        })
+        .catch(err => {
+            reject(err);
+        })
+    });
+};
