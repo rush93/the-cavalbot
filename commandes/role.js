0d7bca44c6a4944185e4ee4d6b687544fc4011c7
Aejii <pierrecadot44@gmail.com>
2018-06-03 23:03:36 +0200

    role dps etc + new people

---
+++ commandes/role.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const Utils = require('../utils');
+var Constants = require('../models/constants');
+module.exports = {
+    role: 'SEND_MESSAGES',
+    helpCat: 'Permet de s\'ajouter un role',
+    help: function (message) {
+        Utils.sendEmbed(message, 0x00AFFF, "Utilisation de la commande role", "", message.author, [{
+            title: Constants.prefix + 'role <@role>',
+            text: "Permet de rejoindre un role (Healer/DPS/Tank/Off-Tank)",
+            grid: false
+        }]);
+    },
+    runCommand: (args, message) => {
+        var nomRole = args.join(' ');
+        var role = ["Healer","DPS","Tank","Off-Tank"];
+        if (role.indexOf(nomRole) == -1 || message.guild.roles.find('name', nomRole) == null) {
+            Utils.reply(message, "Aucun role avec ce nom :O", true);
+            return;
+        }else if(message.member.roles.find('name', nomRole)) {
+        	message.member.removeRole(message.guild.roles.find('name', nomRole));
+            Utils.reply(message, "Suppression du role réussi.", true);
+            return;
+        }else{
+            message.member.addRole(message.guild.roles.find('name', nomRole));
+            Utils.reply(message, "Profite bien de ton nouveau role O^O.", true);
+            return;
+        }
+    }
+}
