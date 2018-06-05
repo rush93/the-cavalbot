ddcfb9ecb6e041638a4a65f27c4c2d1e9cc08e01
Aejii <pierrecadot44@gmail.com>
2018-06-05 23:08:12 +0200

    fix last

--- the-cavalbot.js
+++ the-cavalbot.js
@@ -122,12 +122,12 @@ try {
     moment.locale('fr');
     member.guild.channels.get("402965157011128323").send(`${member.user.username} nous a quitté, il a été avec nous pendant `+ moment.duration(diff, 'minutes').humanize() );
 
-    var clanId = clans.getPlayerClan(message.member).id;
-    var clan = clans.getPlayerClan(message.member);
-    var player = players.getPlayer(message.member.id, clanId);
-    players.resetRank(message.member, clan);
-    players.setPoints(message.member.id, clanId, 0);
-    
+    var clanId = clans.getPlayerClan(member).id;
+    var clan = clans.getPlayerClan(member);
+    var player = players.getPlayer(member.id, clanId);
+    players.resetRank(member, clan);
+    players.setPoints(member.id, clanId, 0);
+
   });
 
   bot.on('guildMemberAdd', member => {
