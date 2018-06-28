23fe5d9aa0e482e1679dec82e07757378bf2a9e1
Aejii <pierrecadot44@gmail.com>
2018-06-28 18:20:10 +0200

    update
    -givepoint modif droit
    -propose cooldown

--- commandes/propose.js
+++ commandes/propose.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const Utils = require('../utils');
 var Constants = require('../models/constants');
 var Players = require('../models/players');
+var moment = require('moment');
+
 module.exports = {
     role: 'SEND_MESSAGES',
     helpCat: 'Permet de demander une personne en mariage',
@@ -36,7 +38,19 @@ module.exports = {
             Utils.reply(message, `Vous l'avez déjà demandé veuillez attendre sa réponse.`, true);
             return;
         }
-        
+        var lastJoin = Players.getPlayers()[message.member.id] ? Players.getPlayers()[message.member.id].cooldownMariage : null;
+        if (lastJoin) {
+            lastJoin = moment(lastJoin);
+            var now = moment();
+            diff = Math.abs(now.diff(lastJoin, 'minutes'));
+            if (diff < Constants.mariageCooldown) {
+                var timeLeft = Constants.mariageCooldown - diff;
+                moment.locale('fr');
+                Utils.reply(message, 'Après une longue et douloureuse séparation vous avez besoin de temps pour vous remettre encore ' + moment.duration(timeLeft, 'minutes').humanize()) + ")";
+                return;
+            }
+        }   
+        Players.setCooldownMariage(message.member);
         Players.setAskFor(message.member, user1);
         Utils.reply(message, `<@!${user1.id}>, <@!${message.member.id}> vous demande en mariage:
     - Voulez-vous accepter ? ( **${Constants.prefix}accept <@!${message.member.id}>** )
